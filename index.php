dc381f2b685052c2e0f6ee209b2bf24aed5af648
Luis Lithgow <lalithgow@gmail.com>
2016-09-21 10:31:19 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>PHP Practice</title>
+  <link rel="stylesheet" href="">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+  <div class="container">
+
+    <?php
+      $person1 = "Jax";
+      $person2 = "Alice";
+      $person3 = "Rhonda";
+
+      $people = array("Jax", "Alice", "Rhonda" => , );
+
+
+
+     ?>
+
+    <form action="process.php" method="post">
+      Enter your name <input type="text" name="theName">
+      <input type="submit" class="btn btn-default" name="">
+    </form>
+
+
+
+
+
+
+
+
+  </div>
+</body>
+</html>
---
+++ process.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Process.php</title>
+  <link rel="stylesheet" href="">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+
+  <div class="container">
+
+    <?php
+      $name = $_POST["theName"];
+      echo "Hello, " .$name ;
+
+
+     ?>
+
+
+
+
+  </div>
+
+
+</body>
+</html>
